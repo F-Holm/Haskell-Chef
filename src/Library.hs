709c0d9622bfddc1075aec16fa1f7051880020b3
F-Holm <moshishafcdodgecharger@gmail.com>
2026-05-30 13:22:00 -0300

    datas y types terminados

--- src/Library.hs
+++ src/Library.hs
@@ -1,3 +1,24 @@
 module Library where
 import PdePreludat
 
+data Componente = UnComponente {
+  ingrediente :: String,
+  cantidad :: Number
+} deriving (Show)
+
+data Plato = UnPlato {
+  dificultad :: Number,
+  componentes :: [Componente]
+} deriving (Show)
+
+type TrucoDeCocina = Plato -> Plato
+
+data Participante = UnParticipante {
+  nombre :: String,
+  trucosDeCocina :: [TrucoDeCocina],
+  especialidad :: Plato
+} deriving (Show)
+
+lacteos :: [String]
+lacteos = ["leche", "queso", "helado"]
+

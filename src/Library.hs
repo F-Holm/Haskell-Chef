526df87d97cd001c876130001a291f29c1ceda40
F-Holm <moshishafcdodgecharger@gmail.com>
2026-05-30 14:17:00 -0300

    Parte A terminada

--- src/Library.hs
+++ src/Library.hs
@@ -22,3 +22,60 @@ data Participante = UnParticipante {
 lacteos :: [String]
 lacteos = ["leche", "queso", "helado"]
 
+-- Parte A
+agregarComponente :: Componente -> Plato -> Plato
+agregarComponente unComponente unPlato = unPlato { componentes = unComponente : componentes unPlato }
+
+endulzar :: Number -> TrucoDeCocina
+endulzar unaCantidad unPlato = agregarComponente (UnComponente "Azucar" unaCantidad) unPlato
+
+salar :: Number -> TrucoDeCocina
+salar unaCantidad unPlato = agregarComponente (UnComponente "Sal" unaCantidad) unPlato
+
+darSabor :: Number -> Number -> TrucoDeCocina
+darSabor unaCantidadDeAzucar unaCantidadDeSal unPlato = endulzar unaCantidadDeAzucar.salar unaCantidadDeSal $ unPlato
+
+duplicarSaborComponente :: Componente -> Componente
+duplicarSaborComponente unComponente = unComponente { cantidad = cantidad unComponente }
+
+duplicarPorcion :: TrucoDeCocina
+duplicarPorcion unPlato = unPlato { componentes = map duplicarSaborComponente.componentes $ unPlato }
+
+esPlatoDificil :: Plato -> Bool
+esPlatoDificil unPlato = (> 7).dificultad $ unPlato
+
+tieneMuchosComponentes :: Plato -> Bool
+tieneMuchosComponentes unPlato = (> 5).length.componentes $ unPlato
+
+esComplejo :: Plato -> Bool
+esComplejo unPlato = esPlatoDificil unPlato && tieneMuchosComponentes unPlato
+
+esComponenteSignificativo :: Componente -> Bool
+esComponenteSignificativo unComponente = (>= 10).cantidad $ unComponente
+
+eliminarComponentesInsignificantes :: Plato -> Plato
+eliminarComponentesInsignificantes unPlato = unPlato { componentes = filter esComponenteSignificativo.componentes $ unPlato }
+
+simplificar :: TrucoDeCocina
+simplificar unPlato
+  | esComplejo unPlato = eliminarComponentesInsignificantes unPlato { dificultad = 5 }
+  | otherwise = unPlato
+
+tieneComponente :: Plato -> String -> Bool
+tieneComponente unPlato unIngrediente = any ((== unIngrediente).ingrediente).componentes $ unPlato
+
+tieneComponentes :: [String] -> Plato -> Bool
+tieneComponentes unosIngredientes unPlato = any (tieneComponente unPlato) unosIngredientes 
+
+esVegano :: Plato -> Bool
+esVegano unPlato = not.tieneComponentes (["carne", "huevos"] ++ lacteos) $ unPlato
+
+esSinTacc :: Plato -> Bool
+esSinTacc unPlato = not.tieneComponente unPlato $ "harina"
+
+tieneMuchaSal :: Componente -> Bool
+tieneMuchaSal unComponente = (> 2).cantidad $ unComponente
+
+noAptoHipertension :: Plato -> Bool
+noAptoHipertension unPlato = any tieneMuchaSal.componentes $ unPlato
+
